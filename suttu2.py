6fc6b581cbfc1f1051b49701a249a3abe41f47a8
Mika Vainio <koti.mika.vainio@gmail.com>
2021-02-11 11:51:40 +0200

    Lisätty tiedostoja ja pieniä muokkauksia

--- suttu2.py
+++ suttu2.py
@@ -1,2 +1,27 @@
 # Toinen suttupaperi
 # Toinen kommentti
+
+def bmi(paino, pituus):
+    """Lasketaan painoideksi jakamalla pituus painon neliöllä
+
+    Args:
+        paino (float): paino kilogrammoina
+        pituus (float): pituus metreinä
+
+    Returns:
+        float: painoindeksi
+    """
+    return paino / pituus**2
+   
+   
+
+oma_bmi = bmi(74, 1.71)
+    
+print('Hoh-hoijaa taas on lihottu, painoindeksi on', oma_bmi)
+
+try:
+  print(x)
+except:
+  print('Something went wrong')
+finally:
+  print('The try except is finished')
